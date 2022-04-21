901f82f4a3ca7cb1cf145bd88fe4e5403ddee941
Denis Zangarov <denisimo_98@yahoo.com>
2022-04-21 13:55:41 +0300

    Startup Working

---
+++ WebApplication/WebApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApplication.Models;
+
+namespace WebApplication.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<WebApplication.Models.Picture> Picture { get; set; }
+    }
+}
---
+++ WebApplication/WebApplication/Models/Picture.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication.Models
+{
+    public class Picture
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Image { get; set; }
+
+        public Picture() {
+        
+        }
+    }
+}
